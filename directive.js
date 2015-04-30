59f7f88c3300b7708a5697fae24e40b1335e47d7
David Vespoli <david.vespoli@gmail.com>
2015-04-29 23:49:09 -0700

    pass in dimensions, comments

--- directive.js
+++ directive.js
@@ -1,14 +1,17 @@
 angular.module('myApp').directive('responsiveImage', function(){    
     // return appropriate source
     return {
-      link: function postLink(scope, element, attr) {
-        var size = 
-          // insert desired measurments or pass in
-          screen.width > 1100 ? attr.desktopSize :
-          screen.width > 767 ? attr.tabletSize :
+      link: function postLink(scope, element, attr, sizes) {
+
+        // sizes = [desktop min -1, tablet min - 1]
+        var sizes = sizes || [1099,767],
+          size = 
+          // get appropriate size
+          screen.width > sizes[0] ? attr.desktopSize :
+          screen.width > sizes[1] ? attr.tabletSize :
           attr.mobileSize;
 
-        //modify pathing
+        //modify pathing as needed
         element.attr("src", attr.originalSrc.replace('/files/','/files/styles/'+size+'/public/'));        
       }
     };
